84267a738710b661948777cf6f80a9aa1136bf92
Muhammed Bashuaib <muhammedbashaib470@gmail.com>
2024-05-04 17:50:31 +0300

    ADD dark to theme data

--- lib/res/theme_data.dart
+++ lib/res/theme_data.dart
@@ -2,7 +2,7 @@ import 'package:bookly_app/res/color_app.dart';
 import 'package:flutter/material.dart';
 
 ThemeData themeData() {
-  return ThemeData().copyWith(
+  return ThemeData.dark().copyWith(
     scaffoldBackgroundColor: MyColors.kPrimaryColor,
   );
 }
